46d9cf8e52ef53939e4fdd279e8cd6faf45c5a38
Luan <luuanreis@gmail.com>
2024-01-20 12:39:21 -0300

    structuring the data

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 // variáveis - variables
-
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
 
-
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  close(){
+    Modal.wrapper.classList.remove('open')
+  }
+}
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -19,21 +25,14 @@ form.onsubmit = event => {
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
   
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  Modal.open()
 }
 
-modalBtnClose.onclick = () =>  modalWrapper.classList.remove('open')
-
+Modal.buttonClose.onclick = () => {
+  Modal.close()
+} 
 
 function IMC (weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
 }
-  
-
-
-
-// 3 maneiras de criar e atribuir função a um evento
-// form.onsubmit = function() {}
-// form.onsubmit = () => {}
-// function handleSubmit () {}
