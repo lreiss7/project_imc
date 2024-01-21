b3fcbfaa45264826353619d4c1294d5c6b6a9774
Luan <luuanreis@gmail.com>
2024-01-21 14:44:28 -0300

    modularizing error alert

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 import { Modal }  from './modal.js'
+import { AlertError} from './alert-error.js'
 
 // variáveis - variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+
 form.onsubmit = event => {
   event.preventDefault()
   
   const weight = inputWeight.value
   const height = inputHeight.value
+
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if (showAlertError) {
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
   
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
   
   Modal.message.innerText = message
   Modal.open()
+}
+
+function notANumber(value) {
+  return isNaN(value) || value == ""
 }
 
 function IMC (weight, height) {
